2c31cd45fde517bb2b1d3d16f4808c93d388e151
agnio12 <33416018+agnio12@users.noreply.github.com>
2017-12-05 21:24:31 +0900

    12.5/9:24

---
+++ WebContent/WEB-INF/lib/action/Action.java
@@ -0,0 +1,9 @@
+package com.shall.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+	
+	public ActionForward doProcess(HttpServletRequest request, HttpServletResponse response);
+}
---
+++ WebContent/WEB-INF/lib/sell/SellDTO.java
@@ -0,0 +1,5 @@
+package com.shall.sell;
+
+public class SellDTO {
+
+}
